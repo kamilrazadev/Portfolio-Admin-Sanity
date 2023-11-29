43df23b983d176374feb423d4fc96ec2e7a64607
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-11-29 14:28:16 +0500

    first commit

---
+++ schemas/pet.js
@@ -0,0 +1,23 @@
+// schemas/pet.js
+export default {
+  name: 'pet',
+  type: 'document',
+  title: 'Pet',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'animal',
+      type: 'string',
+      title: 'Animal',
+    },
+    {
+      name: 'counts',
+      type: 'number',
+      title: 'Counts',
+    },
+  ],
+}
